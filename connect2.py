10aea3254d8d9b8d922b9a689f07c82ab8aca51c
fjavy <161038462+fjavy@users.noreply.github.com>
2024-03-22 14:21:20 +0700

    Add files via upload

---
+++ connect2.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+from bot_logic import gen_emodji
+
+with open("token.txt", "r") as f: #Membaca token dari file token.txt
+    token = f.read() #Menyimpan token ke dalam variabel baru
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def passwd(ctx):
+    await ctx.send(gen_pass(5))
+
+@bot.command()
+async def passwd(ctx):
+    await ctx.send(gen_emodji())
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run(token)
